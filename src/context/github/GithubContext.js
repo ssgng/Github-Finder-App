a05ac40d3934a25af4d3443eac856ac8b7993ecb
Selim Sami <Selim.Genc@hhu.de>
2023-03-19 12:03:51 +0100

    search users activated

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,30 +9,40 @@ const GITHUB_TOKEN=process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users:[],
-        loading:true
+        loading:false
     }
 
     const [state,dispatch] = useReducer(githubReducer, initialState)
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`, {
+    const searchUsers = async (text) => {
+        setLoading()
+
+        const params = new URLSearchParams({
+            q:text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
             headers: {
                 Authorization : `token ${GITHUB_TOKEN}`
             }
         })
 
-        const data = await response.json()
+        const {items} = await response.json()
         
         dispatch({
             type:"GET_USERS",
-            payload:data,
+            payload:items,
         })
     }
+
+    // Set Loading
+
+    const setLoading = () => dispatch({type : "SET_LOADING"})
 
     return <GithubContext.Provider value ={{
         users:state.users,
         loadin:state.loading,
-        fetchUsers
+        searchUsers
     }}>
         {children}
     </GithubContext.Provider>
